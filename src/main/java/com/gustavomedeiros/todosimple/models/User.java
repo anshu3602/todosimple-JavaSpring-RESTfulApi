bd5309410c433d816d8571a506d4392c9716fe3b
Gustavo <gustavomedeiros15@outlook.com.br>
2023-03-07 13:14:17 -0300

    Aplication properties change drive

--- src/main/java/com/gustavomedeiros/todosimple/models/User.java
+++ src/main/java/com/gustavomedeiros/todosimple/models/User.java
@@ -1,7 +1,7 @@
 package com.gustavomedeiros.todosimple.models;
 
-import java.util.ArrayList;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Column;

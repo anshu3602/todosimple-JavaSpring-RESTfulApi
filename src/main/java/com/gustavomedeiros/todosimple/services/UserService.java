244523ace95adbd37cd4a3461951e9331482037f
Gustavo <gustavomedeiros15@outlook.com.br>
2023-03-08 14:24:05 -0300

    Create User Service

---
+++ src/main/java/com/gustavomedeiros/todosimple/services/UserService.java
@@ -0,0 +1,57 @@
+package com.gustavomedeiros.todosimple.services;
+
+import java.util.Optional;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gustavomedeiros.todosimple.models.User;
+import com.gustavomedeiros.todosimple.repositories.TaskRepository;
+import com.gustavomedeiros.todosimple.repositories.UserRepository;
+
+@Service 
+public class UserService {
+    
+    @Autowired // Contructor for service and now have access to both
+    private UserRepository userRepository;
+
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+
+    public User findById(Long id) {
+        Optional<User> user = this.userRepository.findById(id);   // Case user does't exist, will return empty
+        return user.orElseThrow(() -> new RuntimeException(
+            "User not found! id:" + id + ", Type: " + User.class.getName()
+        )); // Return user if user exist, else return a exception and stop application
+        // RunTimeException is use when the exception can be prevented                              
+    }
+
+    @Transactional // Use to Presistent something in the database, to have better control of what is happening in the application
+    public User create(User obj) {
+        obj.setId(null); // The new user can create itself with a ID how already exists, so, let make the Id null
+        obj = this.userRepository.save(obj);
+        this.taskRepository.saveAll(obj.getTasks());
+        return obj;
+    }
+
+    @Transactional // Insert in database need to use Transaction
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        newObj.setPassword(obj.getPassword());
+        return this.userRepository.save(newObj);
+    }
+
+    public void delete(Long id) {
+        findById(id);
+        try {
+            this.userRepository.deleteById(id);
+        } catch (Exception e){
+            throw new RuntimeException("Delete is no possible cause exist related entity! ");
+        }
+    }
+
+}
